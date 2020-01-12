743ad26c5be88e8ebcb64a3198e3843e29f9bb1e
Buhamad <mr.buhamad@hotmail.com>
2020-01-12 19:49:59 +0300

    tesk 2

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def website_test (request):
+	context = {"msg":"Hello World!",
+	"title": "django task 2",
+    "header": "I thnink i finished Task 2!"}
+
+	return render(request, "html_file.html", context)
--- task_02/urls.py
+++ task_02/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from restaurants import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',,name='hello-world'),
+    path('django',views.website_test,name='hello-world'),
 ]
